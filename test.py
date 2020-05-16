1f943ca5ffd503e82bfe2e13689edbe90e45355d
Azu <cxztpcom@gmail.com>
2020-05-16 10:19:02 +0800

    Stylistic improvement

--- test.py
+++ test.py
@@ -11,11 +11,11 @@ class TestGameBoard:
     board2 = TicTacToeBoard(data2)
 
     def test_put(self):
-        self.board.put_1(0, 1)
-        assert self.board.board[0, 1] == 1
+        self.board.put_circle(0, 1)
+        assert self.board.board_values[0, 1] == 1
 
-        self.board.put_neg_1(1, 1)
-        assert self.board.board[1, 1] == -1
+        self.board.put_cross(1, 1)
+        assert self.board.board_values[1, 1] == -1
 
     def test_copy(self):
         dup_board = self.board.copy()
@@ -24,7 +24,7 @@ class TestGameBoard:
         assert dup_board is not self.board
 
         # Test two board have the same content
-        assert np.all(self.board.board == dup_board.board)
+        assert np.all(self.board.board_values == dup_board.board_values)
 
     def test_check_status(self):
         assert self.board2.check_status(0, 2, 1) == 1
--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -5,12 +5,14 @@ The game of TicTacToe
 import numpy as np
 import utils
 
+CIRCLE = 1
+CROSS = -1
+
 
 class TicTacToeBoard:
     """
     The board the holds the tic-tac-toe game
     """
-
     continuous_pieces_to_win = 3
 
     def __init__(self, board: np.ndarray):
@@ -19,28 +21,32 @@ class TicTacToeBoard:
         :param board: A 3 by numpy ndarray, with elements 0 and 1
         """
 
-        self.board = board
+        self.board_values = board
 
         # Our board always has length == width
         self.board_width, self.board_length = board.shape
 
-    def put_1(self, x: int, y: int):
+    @property
+    def board_size(self):
+        return self.board_width
+
+    def put_circle(self, x: int, y: int):
         """
         Put the 1 chess piece at the given position of the board
         :param x: Horizontal index
         :param y: Vertical index
         :return:
         """
-        self.board[x, y] = 1
+        self.board_values[x, y] = CIRCLE
 
-    def put_neg_1(self, x: int, y: int):
+    def put_cross(self, x: int, y: int):
         """
         Put chess piece -1 at the given position of the board
         :param x: Horizontal index
         :param y: Vertical index
         :return:
         """
-        self.board[x, y] = -1
+        self.board_values[x, y] = CROSS
 
     def check_status(self, x: int, y: int, piece: int) -> int:
         """
@@ -51,31 +57,87 @@ class TicTacToeBoard:
         winnable = piece * self.continuous_pieces_to_win
 
         # Horizontally continuous pieces
-        if np.sum(self.board[x, :]) == winnable:
+        if np.sum(self.board_values[x, :]) == winnable:
             return piece
 
         # Vertically continuous pieces
-        if np.sum(self.board[:, y]) == winnable:
+        if np.sum(self.board_values[:, y]) == winnable:
             return piece
 
         # Diagonally continuous pieces
         if x == y:
-            upper_diagonal_values = np.sum([self.board[self.board_length - 1 - i, i] for i in range(self.board_width)])
-            lower_diagonal_values = np.sum([self.board[i, i] for i in range(self.board_width)])
+            upper_diagonal_values = np.sum([self.board_values[self.board_length - 1 - i, i] for i in range(self.board_width)])
+            lower_diagonal_values = np.sum([self.board_values[i, i] for i in range(self.board_width)])
             if upper_diagonal_values == winnable or lower_diagonal_values == winnable:
                 return piece
 
         # Game is not over
         return 0
 
+
     def copy(self):
         """
         Copy the board
         :return:  A new instance of the board
         """
-        data = self.board.copy()
+        data = self.board_values.copy()
         return type(self)(data)
 
+    @classmethod
+    def create_empty_board(cls, size=3):
+        board_value = np.array([[0]*3 for _ in range(size)])
+        return cls(board_value)
+
     def __str__(self):
-        string_representation = utils.get_string_grid(self.board_width, self.board_length, self.board)
+        string_representation = utils.get_string_grid(self.board_width, self.board_length, self.board_values)
         return string_representation
+
+
+class TicTacToeGame:
+    """
+    The tic tac toe game class, determining who is the next
+    player and the status of a game
+    """
+    def __init__(self, game_board: TicTacToeBoard, first_player=1):
+        self.game_board = game_board
+        self.current_to_play = first_player
+
+        self.num_steps_played = 0
+
+        self.game_running = False
+
+    def next_player(self):
+        if self.current_to_play == CROSS:
+            return CIRCLE
+
+        return CROSS
+
+    def check_tied(self):
+        board_size = self.game_board.board_size
+        if board_size ** 2 == self.num_steps_played:
+            return True
+
+        return False
+
+    def put_piece(self, x: int, y: int):
+        """
+        Put the piece of current player at the given position
+
+        :param x: Row index of the chess
+        :param y: Column index of the chess
+        :return:
+        """
+        if self.game_board.board_values[x, y] != 0:
+            raise ValueError("You must select a empty slot!")
+
+        self.game_board.board_values[x, y] = self.current_to_play
+
+    def start_game(self):
+        print("Game started!")
+        self.game_running = True
+        while self.game_running:
+            pass
+
+
+
+
